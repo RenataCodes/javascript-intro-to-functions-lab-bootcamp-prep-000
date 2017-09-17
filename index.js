07f2799a7f11758928d464f70cbf503ea015200b
RenataCodes <psych.coaching@gmail.com>
2017-09-17 13:43:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === "string") {
-  console.log( "I can\'t hear you!")
-} else {
-  console.log("over")
+if (string.toLowerCase() === string) {
+  console.log("I can\'t hear you!")
+} 
+  
 }
